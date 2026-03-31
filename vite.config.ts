688140a8d38cc979558b8e16bc1c8dd74b64a71d
Pushkar Gooljar <gooljarpushkar@gmail.com>
2026-03-31 21:13:13 +0400

    updated battleship estimator

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ['localhost', '127.0.0.1', '11bf-102-119-100-75.ngrok-free.app', 'pushthecar.com']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
